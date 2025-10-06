ede8d8c944aec2181dd40f1490cece0d676a23fd
Baritra1 <aritraandbanerjee@gmail.com>
2025-10-05 21:55:43 -0400

    Add generators

---
+++ generation/generate_graph.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+length = np.random.randint(2, 6)
+
+q = np.random.randint(1, 101)
+
+d = np.zeros(length, dtype=int)
+if length > 1:
+    d[1:] = np.random.randint(0, q + 1, size=length - 1)
+
+k = np.random.randint(length, length + 50)  # k >= len(d)
+
+print("d:", d)
+print("q:", q)
+print("k:", k)
